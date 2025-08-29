35f600c7f4985d15b55bba47fc2c7719d52a6a25
dossgod-56 <emmanuelosaretin250@gmail.com>
2025-08-29 15:47:28 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ function convertToBool(text, fault = "true") {
 }
 
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "Enter your sesson id with out mulvin~",
-  OWNER_NUM: process.env.OWNER_NUM || "94726228243",
+  SESSION_ID: process.env.SESSION_ID || "rG5jCRID#0u-IUnC4msvesEP7v7iySrh_P3-8M1L-y79DWVO80fY",
+  OWNER_NUM: process.env.OWNER_NUM || "2348067590099",
   PREFIX: process.env.PREFIX || ".",
   AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
   AUTO_REACT_STATUS: process.env.AUTO_REACT_STATUS || "true",
